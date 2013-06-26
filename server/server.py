f49880c3e17b66e2523a2d45b5a6d67e795532dd
obnoxious <0_bn0x@hushmail.com>
2013-06-26 11:23:29 -0700

    Fucked up moving files.

--- server/server.py
+++ server/server.py
@@ -1,20 +1,63 @@
 import socket
 import threading
-import json
 from threading import Thread
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind(('', 35931))
+serverSocket.listen(1337)
 
-clientSocket = socket.socket()
-clientSocket.connect(('127.0.0.1', 35931))
+registeredNicks = {}
+activeGamesList = {}
 
-def socketSendData():
-	userInput = raw_input()
-	clientSocket.send(userInput)
+def registerNick(ip, nickName):
+	global registeredNicks
+	registeredNicks[ip] = nickName
+	return '%s is now registered as: %s\n' % (ip, nickName)
 
-def socketWaitForData():
-	data = clientSocket.recv(1024)
-	print data
+def gamesList():
+	return '\n'.join(activeGamesList)
 
+def createGame(gameName):
+	global activeGamesList
+	if address[0] in registeredNicks:
+		pass
+	else:
+		return 'NOT REGISTERED'
+	try:
+		gameName = ' '.join(gameName).strip('\n')
+	except:
+		gamename = gameName.strip('\n')
+
+	if gameName.strip('\n') in activeGamesList:
+		return '%s EXIST\n' % gameName
+	else:
+		activeGamesList[gameName] = gameName
+		return '%s CREATED\n' % gameName
+	
+	return 'Failed to create game.\n'
+
+def handleConnection():
+	while True:
+		receivedData = clientSocket.recv(1024)
+		try:
+			receivedData = receivedData.split()
+			
+			if receivedData[0] == 'NICK':
+				clientSocket.send(registerNick(address[0], receivedData[1]))
+			
+			if receivedData[0] == 'LIST':
+				global activeGamesList
+				clientSocket.send(gamesList())
+			
+			if receivedData[0] == 'CREATE':
+				if len(receivedData) > 1:
+					tempData = createGame(receivedData[1:])
+				else:
+					tempData = createGame(receivedData[1])
+				clientSocket.send(tempData)
+
+		except:
+			clientSocket.send('Failed parsing command.\n')
 while 1:
-	thread = Thread(target = socketWaitForData)
-	thread.start()
-	socketSendData()
+    (clientSocket, address) = serverSocket.accept()
+    thread = Thread(target = handleConnection)
+    thread.start()
